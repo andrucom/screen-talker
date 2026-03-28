bf1094f8085ecc5875a2315440b257cc8f451b79
Бендер <andrucom123@gmail.com>
2026-03-29 02:45:43 +0300

    Update keyboard.gd

--- scripts/keyboard.gd
+++ scripts/keyboard.gd
@@ -8,10 +8,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-
 	pass
 	
-	
+
+#--------------------#
+# Функции для самого процесса нажатия и отжатия 
 func push_button(node: String):
 	var body = get_node(node)
 	body.position -= Vector3(0,0.01,0)
@@ -20,53 +21,14 @@ func unpush_button(node: String):
 	var body = get_node(node)
 	body.position += Vector3(0,0.01,0)
 
+# Нажатие и отжатие кнопок на клавиатуре. !На 3д модели должны совпадать keycode с названием мешей
 func _input(event: InputEvent) -> void:
 		if event is InputEventKey and event.is_pressed() and not pressed_handled.get(event.keycode,false):
-			match event.keycode:
-				KEY_W:
-					push_button("w")
-				KEY_Q:
-					push_button("q")
-				KEY_E:
-					push_button("e")
-				
-				KEY_A:
-					push_button("a")
-				KEY_S:
-					push_button("s")
-				KEY_D:
-					push_button("d")
-
-				KEY_Z:
-					push_button("z")
-				KEY_X:
-					push_button("x")
-				KEY_C:
-					push_button("c")
+			push_button(OS.get_keycode_string(event.keycode).to_lower())
 			pressed_handled[event.keycode] = true
-
+			
 		if event is InputEventKey and event.is_released() and pressed_handled.get(event.keycode,true):
-			match event.keycode:
-				KEY_W:
-					unpush_button("w")
-				KEY_Q:
-					unpush_button("q")
-				KEY_E:
-					unpush_button("e")
-				
-				KEY_A:
-					unpush_button("a")
-				KEY_S:
-					unpush_button("s")
-				KEY_D:
-					unpush_button("d")
-
-				KEY_Z:
-					unpush_button("z")
-				KEY_X:
-					unpush_button("x")
-				KEY_C:
-					unpush_button("c")
+			unpush_button(OS.get_keycode_string(event.keycode).to_lower())
 			pressed_handled[event.keycode] = false
 
 		#if event is InputEventKey:
