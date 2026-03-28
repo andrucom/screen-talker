465e8e853732519fec283d99bfd976891d33f606
Бендер <andrucom123@gmail.com>
2026-03-29 02:21:33 +0300

    Клавиатура основа

--- scripts/keyboard.gd
+++ scripts/keyboard.gd
@@ -12,29 +12,61 @@ func _process(delta: float) -> void:
 	pass
 	
 	
+func push_button(node: String):
+	var body = get_node(node)
+	body.position -= Vector3(0,0.01,0)
+
+func unpush_button(node: String):
+	var body = get_node(node)
+	body.position += Vector3(0,0.01,0)
+
 func _input(event: InputEvent) -> void:
 		if event is InputEventKey and event.is_pressed() and not pressed_handled.get(event.keycode,false):
 			match event.keycode:
 				KEY_W:
-						var body = get_node('w')
-						body.position -= Vector3(0,0.01,0)
-						print(OS.get_keycode_string(event.keycode))
+					push_button("w")
 				KEY_Q:
-					var body = get_node('q')
-					body.position -= Vector3(0,0.01,0)
-					print(OS.get_keycode_string(event.keycode))
+					push_button("q")
+				KEY_E:
+					push_button("e")
+				
+				KEY_A:
+					push_button("a")
+				KEY_S:
+					push_button("s")
+				KEY_D:
+					push_button("d")
+
+				KEY_Z:
+					push_button("z")
+				KEY_X:
+					push_button("x")
+				KEY_C:
+					push_button("c")
 			pressed_handled[event.keycode] = true
 
 		if event is InputEventKey and event.is_released() and pressed_handled.get(event.keycode,true):
 			match event.keycode:
 				KEY_W:
-						var body = get_node('w')
-						body.position += Vector3(0,0.01,0)
-						print(OS.get_keycode_string(event.keycode))
+					unpush_button("w")
 				KEY_Q:
-					var body = get_node('q')
-					body.position += Vector3(0,0.01,0)
-					print(OS.get_keycode_string(event.keycode))
+					unpush_button("q")
+				KEY_E:
+					unpush_button("e")
+				
+				KEY_A:
+					unpush_button("a")
+				KEY_S:
+					unpush_button("s")
+				KEY_D:
+					unpush_button("d")
+
+				KEY_Z:
+					unpush_button("z")
+				KEY_X:
+					unpush_button("x")
+				KEY_C:
+					unpush_button("c")
 			pressed_handled[event.keycode] = false
 
 		#if event is InputEventKey:
