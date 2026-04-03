52f09d5c82f16a5f1e050363d375e3bc0a10a3e8
Бендер <andrucom123@gmail.com>
2026-04-03 23:09:55 +0300

    terminal
    - _animated_text

--- scripts/terminal.gd
+++ scripts/terminal.gd
@@ -1,22 +1,20 @@
 extends Control
 @onready var labelR = $RichTextLabel
-@onready var label = $Label
+@onready var ver = $LabelVER
 @onready var label2 = $Label2
 const VERSION_DATA = preload("res://version.tres")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Убедитесь, что текст полностью невидим в начале
-	labelR.visible_ratio = 0.0
-	# Создаем и запускаем анимацию
-	var tween = create_tween()
-	# Анимируем свойство visible_ratio от 0 до 1 за 3 секунды
-	tween.tween_property(labelR, "visible_ratio", 1.0, 1.0)
-	
-	label.text = "VER: " + VERSION_DATA.version
+	ver.text ="VER:  " + VERSION_DATA.version
+	_animated_text(labelR,3)
 
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	label2.text = "TIME: " + Time.get_time_string_from_system()
 	pass
+
+
+func _animated_text(label, time):
+	label.visible_ratio = 0.0
+	var tween = create_tween()
+	tween.tween_property(label,"visible_ratio",1,time)
