dde768e0b26807b087418c7522f9af066f1b52d3
Dadaskis <dadaskis@gmail.com>
2026-04-03 18:07:30 +0300

    Basic terminal grid

---
+++ scripts/terminal_letter_grid.gd
@@ -0,0 +1,90 @@
+@tool
+
+extends GridContainer
+
+@export var width: = 0: 
+	set(value):
+		width = value
+		if Engine.is_editor_hint():
+			update_grid()
+
+@export var height: = 0: 
+	set(value):
+		height = value
+		if Engine.is_editor_hint():
+			update_grid()
+
+func update_grid():
+	var preset = get_node("_PRESET")
+	preset.visible = true
+	
+	for child in get_children():
+		if child == preset:
+			continue
+		remove_child(child)
+		child.queue_free()
+	
+	columns = width
+	for x in range(width):
+		for y in range(height):
+			var dupe = preset.duplicate()
+			dupe.name = str(x) + ":" + str(y)
+			add_child(dupe)
+	
+	preset.visible = false
+
+func set_letter(
+	x: int, 
+	y: int, 
+	letter: String, 
+	color: = Color.WHITE, 
+	color_background: = Color.BLACK
+):
+	if x < 0 or x >= width:
+		return
+	if y < 0 or y >= height:
+		return
+	
+	if len(letter) == 0:
+		letter = " "
+	letter = letter[0]
+	
+	var rect: = get_node(str(x) + ":" + str(y)) as ColorRect
+	rect.color = color_background
+	
+	var letter_label: = rect.get_child(0) as Label
+	letter_label.self_modulate = color
+	letter_label.text = letter
+
+func get_letter_char(x: int, y: int) -> String:
+	if x < 0 or x >= width:
+		return " "
+	if y < 0 or y >= height:
+		return " "
+	
+	var rect: = get_node(str(x) + ":" + str(y)) as ColorRect
+	var letter_label: = rect.get_child(0) as Label
+	return letter_label.text
+
+func get_letter_color(x: int, y: int) -> Color:
+	if x < 0 or x >= width:
+		return Color.BLACK
+	if y < 0 or y >= height:
+		return Color.BLACK
+	
+	var rect: = get_node(str(x) + ":" + str(y)) as ColorRect
+	var letter_label: = rect.get_child(0) as Label
+	return letter_label.self_modulate
+
+func get_letter_background_color(x: int, y: int) -> Color:
+	if x < 0 or x >= width:
+		return Color.BLACK
+	if y < 0 or y >= height:
+		return Color.BLACK
+	
+	var rect: = get_node(str(x) + ":" + str(y)) as ColorRect
+	return rect.color
+
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		update_grid()
