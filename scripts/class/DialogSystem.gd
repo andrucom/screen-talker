e97efe60b0e55be6ba1dd815d414cfc37a56314e
Бендер <andrucom123@gmail.com>
2026-04-09 18:55:38 +0300

    Dialog system

---
+++ scripts/class/DialogSystem.gd
@@ -0,0 +1,39 @@
+class_name DialogSystem
+extends Node
+
+var path =  "res://text/text_ru.json"
+var dialogues_data = []
+
+func _enter_tree() -> void:
+	load_dialogues(path)
+
+func load_dialogues(file_path: String):
+	# Проверяем, существует ли файл
+	if not FileAccess.file_exists(file_path):
+		print("Файл не найден: ", file_path)
+		return
+	
+	# Открываем файл
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file == null:
+		print("Не удалось открыть файл: ", file_path)
+		return
+	
+	# Читаем и парсим
+	var content = file.get_as_text()
+	file.close()
+	
+	var result = JSON.parse_string(content)
+	
+	if result == null:
+		print("Ошибка парсинга JSON")
+		dialogues_data = []
+	else:
+		dialogues_data = result
+		print("Загружено диалогов: ", dialogues_data.size())
+
+func get_text_by_id(id: String) -> String:
+	for dialogue in dialogues_data:
+		if dialogue.get("id") == id:
+			return dialogue.get("text", "")
+	return ""  # Если ID не найден
--- scripts/terminal.gd
+++ scripts/terminal.gd
@@ -7,12 +7,21 @@ extends Control
 @onready var logo = $logo
 @onready var ps = $logo/placeholder
 @onready var progressbar = $logo/ProgressBar
+@onready var path =  "res://text/text_ru.json"
+
+var DS
 
 const VERSION_DATA = preload("res://version.tres")
 
 func _ready() -> void:
-	LabelVER.text ="VER:  " + VERSION_DATA.version
 	start()
+	LabelMAIN.text = DS.get_text_by_id("start")
+	
+	await GTime.delay(10)
+
+	_animated_text(LabelMAIN, 2)
+	LabelMAIN.text = DS.get_text_by_id("start2")
+	
 	
 func _process(delta: float) -> void:
 	LabelTIME.text = "TIME: " + Time.get_time_string_from_system()
@@ -23,6 +32,14 @@ func _animated_text(label, time):
 	tween.tween_property(label,"visible_ratio",1,time)
 
 func start():
+	# init ver
+	LabelVER.text ="VER:  " + VERSION_DATA.version
+	
+	# init DialogSystem
+	DS = DialogSystem.new()
+	LabelMAIN.add_child(DS)
+	
+	# Screen logo
 	await GTime.delay(6)
 	ps.visible = false
 	progressbar.visible = true
