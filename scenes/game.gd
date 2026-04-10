43afcdd4d8d2e75945d593ed0c796eb894967b27
Бендер <andrucom123@gmail.com>
2026-04-11 00:13:06 +0300

    TextEdit test

--- scenes/game.gd
+++ scenes/game.gd
@@ -5,11 +5,13 @@ extends Control
 @onready var LabelRULE = $main/LabelRULE
 @onready var LabelMAIN = $main/LabelMAIN
 @onready var LabelCHOISE = $main/LabelCHOISE
+@onready var TextE = $main/TextEdit
+
 var DS = DialogSystem.new()
 var add = false
 
-
 func _ready() -> void:
+	TextE.grab_focus()
 	main.add_child(DS)
 	
 	dev.visible = false
@@ -18,9 +20,8 @@ func _ready() -> void:
 	LabelRULE.text = DS.get_text_by_id("rule")
 	LabelMAIN.text = "score: " + str(Globals.score)
 
-
-
 func _physics_process(delta: float) -> void:
+
 	score_add()
 	update_text()
 
@@ -28,7 +29,6 @@ func update_text():
 	LabelMAIN.text = "score: " + str(Globals.score)
 
 func score_add():
-	
 	if add == false:
 		add = true
 		await  GTime.delay(1)
