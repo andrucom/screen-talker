04ddd2bfd2bc1a03695a51290158e440c082998f
Бендер <andrucom123@gmail.com>
2026-04-14 01:07:58 +0300

    labirint test

---
+++ scripts/character_2d.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 3000
+
+
+func _physics_process(delta: float) -> void:
+	if Input:
+		
+		var horizontal := Input.get_axis("left", "right")
+		var vertical := Input.get_axis("up", "down")
+		
+		var motion = Vector2(horizontal, vertical) * SPEED * delta
+		move_and_collide(motion)
+	
+	#var direction := Input.get_axis("left", "right")
+	#
+	#if direction:
+		#print(direction)
+		#self.position.x += direction * SPEED
+		
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
