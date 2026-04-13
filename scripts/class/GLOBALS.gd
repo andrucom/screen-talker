cc842f53dfa673ca7c2946ba34a169d1fbaea44b
Бендер <andrucom123@gmail.com>
2026-04-13 23:23:15 +0300

    game test

--- scripts/class/GLOBALS.gd
+++ scripts/class/GLOBALS.gd
@@ -5,5 +5,31 @@ var dot = false
 var choice = ""
 
 var score = 0
-var multiplayer_1 = 1.0
-var cost_multiplayer_1 = 1
+#var multiplayer_1 = 1.0
+#var cost_multiplayer_1 = 1
+
+#var values = {
+	#"multiplayer_1": {"value": 1.0, "cost": 1}
+#}
+
+var upgrades = {
+	#				Умножитель		Цена		Умножитель умнож	Умножение цены
+	"multiplayer_1": {"value": 1.0, "cost": 20, "mult_value": 1.3, "mult_cost":1.3},
+	"multiplayer_2": {"value": 1.0, "cost": 100, "mult_value": 4, "mult_cost":2},
+	"multiplayer_3": {"value": 1.0, "cost": 1000, "mult_value": 10, "mult_cost":10},
+}
+
+func upgrade():
+	for upgrade_key in upgrades:
+		var upgrade_data = upgrades[upgrade_key]
+		
+		if (score - upgrade_data.cost) > 0:
+			score -= upgrade_data.cost
+			upgrade_data.value *= upgrade_data.mult_value
+			upgrade_data.cost *= upgrade_data.mult_cost
+
+
+	#if (G.score - G.cost_multiplayer_1) > 0:
+		#G.score -= G.cost_multiplayer_1 
+		#G.multiplayer_1 *= 1.1
+		#G.cost_multiplayer_1 *= 2
--- scripts/game.gd
+++ scripts/game.gd
@@ -30,8 +30,17 @@ func _on_pc_input_connect() -> void:
 	game_handler()
 
 func update_text():
-	LabelMAIN.text = "score: " + str(int(G.score)) + "\nmultiplayer_1: " + str(snapped(G.multiplayer_1,0.01)) + "\t\t\t\t[COST: " + str(G.cost_multiplayer_1) + "]"
-	TextAnimator._typeware(LabelMAIN,0.1)
+	LabelMAIN.text = ">>score: " + str(int(G.score)) \
+	+ "\n\n\nmultiplayer_1: " \
+	+ str(snapped(G.upgrades["multiplayer_1"].value, 0.01)) + "\t\t\t\t[COST: " + str(G.upgrades["multiplayer_1"].cost) + "]" \
+	
+	+ "\nmultiplayer_2: " \
+	+ str(snapped(G.upgrades["multiplayer_2"].value, 0.01)) + "\t\t\t\t[COST: " + str(G.upgrades["multiplayer_2"].cost) + "]" \
+	
+	+ "\nmultiplayer_3: " \
+	+ str(snapped(G.upgrades["multiplayer_3"].value, 0.01)) + "\t\t\t\t[COST: " + str(G.upgrades["multiplayer_3"].cost) + "]"
+	
+	#TextAnimator._typeware(LabelMAIN,0.1)
 
 #only game scene
 func _input(event: InputEvent) -> void:
@@ -39,17 +48,31 @@ func _input(event: InputEvent) -> void:
 		game_handler()
 
 func game_handler():
-	if self.visible != false:
-		upgrade()
+	if self.visible:
 		score_add()
+		G.upgrade()
+		#upgrade(G.multiplayer_1, G.cost_multiplayer_1)
 		update_text()
 
-func upgrade():
-	if (G.score - G.cost_multiplayer_1) > 0:
-		G.score -= G.cost_multiplayer_1 
-		G.multiplayer_1 *= 1.1
-		G.cost_multiplayer_1 *= 2
+#func upgrade(multiplayer, cost_multiplayer):
+	#if (G.score - cost_multiplayer) > 0:
+		#G.score -= cost_multiplayer 
+		#multiplayer *= 1.1
+		#cost_multiplayer *= 2
+	#return [multiplayer, cost_multiplayer]
+
+#func upgrade_1():
+	#if (G.score - G.cost_multiplayer_1) > 0:
+		#G.score -= G.cost_multiplayer_1 
+		#G.multiplayer_1 *= 1.1
+		#G.cost_multiplayer_1 *= 2
 
 func score_add():
-	G.score += 1 * G.multiplayer_1
+	var mult = 0
+	for i in G.upgrades:
+		print(">>" + str(G.upgrades[i].value) + "  " + str(i))
+		mult += G.upgrades[i].value
+	print(mult/G.upgrades.size())
+
+	G.score += 1 * (mult / G.upgrades.size())
 		
