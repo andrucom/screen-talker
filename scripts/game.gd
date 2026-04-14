c4e1e7d3c2f5d2ad94026f74fb272f864fcfc44e
Бендер <andrucom123@gmail.com>
2026-04-14 21:54:31 +0300

    remove clicker game

--- scripts/game.gd
+++ scripts/game.gd
@@ -27,52 +27,6 @@ func _physics_process(delta: float) -> void:
 	pass
 	
 func _on_pc_input_connect() -> void:
-	game_handler()
+	pass
 
-func update_text():
-	LabelMAIN.text = ">>score: " + str(int(G.score)) \
-	+ "\n\n\nmultiplayer_1: " \
-	+ str(snapped(G.upgrades["multiplayer_1"].value, 0.01)) + "\t\t\t\t[COST: " + str(G.upgrades["multiplayer_1"].cost) + "]" \
-	
-	+ "\nmultiplayer_2: " \
-	+ str(snapped(G.upgrades["multiplayer_2"].value, 0.01)) + "\t\t\t\t[COST: " + str(G.upgrades["multiplayer_2"].cost) + "]" \
-	
-	+ "\nmultiplayer_3: " \
-	+ str(snapped(G.upgrades["multiplayer_3"].value, 0.01)) + "\t\t\t\t[COST: " + str(G.upgrades["multiplayer_3"].cost) + "]"
-	
-	#TextAnimator._typeware(LabelMAIN,0.1)
-
-#only game scene
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_released():
-		game_handler()
-
-func game_handler():
-	if self.visible:
-		score_add()
-		G.upgrade()
-		#upgrade(G.multiplayer_1, G.cost_multiplayer_1)
-		update_text()
-
-#func upgrade(multiplayer, cost_multiplayer):
-	#if (G.score - cost_multiplayer) > 0:
-		#G.score -= cost_multiplayer 
-		#multiplayer *= 1.1
-		#cost_multiplayer *= 2
-	#return [multiplayer, cost_multiplayer]
-
-#func upgrade_1():
-	#if (G.score - G.cost_multiplayer_1) > 0:
-		#G.score -= G.cost_multiplayer_1 
-		#G.multiplayer_1 *= 1.1
-		#G.cost_multiplayer_1 *= 2
-
-func score_add():
-	var mult = 0
-	for i in G.upgrades:
-		print(">>" + str(G.upgrades[i].value) + "  " + str(i))
-		mult += G.upgrades[i].value
-	print(mult/G.upgrades.size())
-
-	G.score += 1 * (mult / G.upgrades.size())
 		
