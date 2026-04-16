3645053ce9d2c15a1976ed168b4831e380925508
Бендер <andrucom123@gmail.com>
2026-04-16 14:19:28 +0300

    tilemap system

--- scripts/character_2d.gd
+++ scripts/character_2d.gd
@@ -1,47 +1,34 @@
 extends CharacterBody2D
 
+@onready var title_map = get_node("../lvl1")
 
 @export var SPEED = 0.1
 var original_SPEED = SPEED
 var moving
 var move_direction
+var original_position = self.position
 
 func _physics_process(delta: float) -> void:
 	# Запоминаем направление при нажатии
-	if Input:
+	if Input.is_anything_pressed():
 		var horizontal = Input.get_axis("left", "right")
 		var vertical = Input.get_axis("up", "down")
 		move_direction = Vector2(horizontal, vertical).normalized()
 		moving = true
 	
-	# Двигаемся пока не столкнемся
 	if moving:
 		var motion = move_direction * SPEED * delta
 		var collision = move_and_collide(motion)
-		
+		check_tilemap()
 		if collision:
-			moving = false  # Останавливаемся при столкновении
+			moving = false  
+
+func check_tilemap():
+	var cell_pos = title_map.local_to_map(global_position)
+	var tile_data =  title_map.get_cell_tile_data(cell_pos)
+	var custom_value = tile_data.get_custom_data("Finish")
 	
-	#var direction := Input.get_axis("left", "right")
-	#
-	#if direction:
-		#print(direction)
-		#self.position.x += direction * SPEED
+	if  tile_data != null and custom_value == true:
+		await GTime.delay(0.1)
+		self.position = original_position
 		
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
